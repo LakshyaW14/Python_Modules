3dd994854832063290b4a346af9729aaba87bad8
Lakshya Wagh <lakshyawagh14@gmail.com>
2026-03-27 22:35:48 +0530

    add pandas.py

---
+++ Pandas/pandas.py
@@ -0,0 +1,14 @@
+# Pandas is a Python library used for working with data sets.
+# It has functions for analyzing, cleaning, exploring, and manipulating data.
+
+# Why Use Pandas?
+# Pandas allows us to analyze big data and make conclusions based on statistical theories.
+
+# Pandas can clean messy data sets, and make them readable and relevant
+# Installation of Pandas
+# C:\Users\Your Name>pip install pandas
+
+import pandas as pd
+
+# The version string is stored under __version__ attribute.
+print(pd.__version__)
